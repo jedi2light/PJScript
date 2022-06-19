90dc4f7af3482b53ccb73fbf06080b7a1fd7a17d
Stoian Minaiev <stoyan.minaev@gmail.com>
2022-06-19 03:09:52 +0300

    Export newly added Expression type

--- pjscript/models/expression/__init__.py
+++ pjscript/models/expression/__init__.py
@@ -5,3 +5,4 @@ from .assignment import AssignmentExpression
 from .objectcall import ObjectCallExpression
 from .membercall import MemberCallExpression
 from .memberaccess import MemberAccessExpression
+from .memberassignment import MemberAssignmentExpression
