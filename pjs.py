006e0f5ca0bef3feac640a978e8dfe908a55c289
Stoian Minaiev <stoyan.minaev@gmail.com>
2022-06-19 22:59:25 +0300

    Add --json-mode/--dump-mode to serialize/print AST to both string and JSON

--- pjs.py
+++ pjs.py
@@ -2,18 +2,26 @@
 
 """PJScript core script"""
 
+import json
 import argparse
-from pjscript.mode.dump import DumpMode
 from pjscript.syntax.lexer import Lexer
 from pjscript.syntax.parser import Parser
 
 parser = argparse.ArgumentParser('PJScript')
 parser.add_argument('source', nargs='?', default='')
+parser.add_argument('--dump-mode', '-d',
+                    help='Prints an AST pseudo code', action='store_true')
+parser.add_argument('--json-mode', '-j',
+                    help='Serializes an AST to JSON', action='store_true')
 
 if __name__ == '__main__':
 
     args = parser.parse_args()
     if not args.source:
-        print('REPL mode is not implemented yet')
+        print('REPL is not implemented yet')
     with open(args.source, 'r', encoding='utf-8') as source_code_reader:
-        DumpMode(Parser(Lexer(source_code_reader.read()).lexed()).parsed()).dump()
+        parsed = Parser(Lexer(source_code_reader.read()).lexed()).parsed()
+        if args.dump_mode:
+            print(parsed)  # <-- use built-in string Program serialization
+        if args.json_mode:
+            print(json.dumps(parsed.to_dict()))  # dumps out a JSON string
