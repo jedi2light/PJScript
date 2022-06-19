9dfa40102032f083e933b06234294a793f4d7770
Stoian Minaiev <stoyan.minaev@gmail.com>
2022-06-19 22:34:53 +0300

    Update examples/startup.tjs and realted files

--- examples/startup.cpp
+++ examples/startup.cpp
@@ -1,9 +1,13 @@
-#include <pjs>
+// examples/startup.tjs
 
-int main() {
-    Value& foo = ObjectValue();
-    Value& bar = foo;
-    foo.set('name', StringValue("User"));
-    console.log(StringValue("Hello, "), foo.get("name"));
-    return 0;
+Value& startup(Environment& _env) {
+    _env.set("xyz", StringValue("This gonna be treated as 'var xyz = ...' form"), mutable=true);
+    _env.get("xyz");
+    _env.set("foo", ObjectValue(), mutable=true);
+    _env.set("foo", ObjectValue(), mutable=true);
+    _env.set("foo", ObjectValue::AsInstance(), mutable=true);
+    _env.set("bar", _env.get("foo"), mutable=false);
+    _env.get("bar").set("name", StringValue("User"), mutable=true);
+    _env.get("bar").get("name");
+    return Console::Log(StringValue("Hello, ", _env.get("bar").get("name"), _env.get("bar").get("name").toString()));
 }
