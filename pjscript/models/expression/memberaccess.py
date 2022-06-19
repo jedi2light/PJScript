f36eb4f6074c6e03e8d7f8460a30730079eea73c
Stoian Minaiev <stoyan.minaev@gmail.com>
2022-06-19 22:51:47 +0300

    Add two child classes for base Access classes

--- pjscript/models/expression/memberaccess.py
+++ pjscript/models/expression/memberaccess.py
@@ -1,35 +1,9 @@
 """PJScript MemberAccessExpression"""
 
-from pjscript.models.expression.base import BaseExpression
-from pjscript.models.literal.identifier import IdentifierLiteral
+from pjscript.models.expression.access \
+    import AccessExpression
 
 
-class MemberAccessExpression(BaseExpression):
+class MemberAccessExpression(AccessExpression):
 
     """MemberAccessExpression class"""
-
-    _name: IdentifierLiteral
-
-    def __init__(self, name: IdentifierLiteral) -> None:
-
-        """Instantiate MemberAccessExpression"""
-
-        self._name = name
-
-    def name(self) -> IdentifierLiteral:
-
-        """Returns MemberAccessExpression name"""
-
-        return self._name
-
-    def __repr__(self) -> str:
-
-        """Debugging simplified"""
-
-        return self.__str__()
-
-    def __str__(self) -> str:
-
-        """Custom serializer made to simplify debugging"""
-
-        return f'{self.__class__.__name__}({self.name()})'
---
+++ pjscript/models/expression/scopedaccess.py
@@ -0,0 +1,9 @@
+"""PJScript ScopedAccessExpression"""
+
+from pjscript.models.expression.access \
+    import AccessExpression
+
+
+class ScopedAccessExpression(AccessExpression):
+
+    """ScopedAccessExpression class"""
