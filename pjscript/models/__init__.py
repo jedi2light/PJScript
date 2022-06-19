330e2d390b30ddb605337d84fee03ea397f177b2
Stoian Minaiev <stoyan.minaev@gmail.com>
2022-06-19 22:55:38 +0300

    Update sub-package

--- pjscript/models/__init__.py
+++ pjscript/models/__init__.py
@@ -4,3 +4,5 @@ from .base import BaseModel
 from .literal.base import BaseLiteral
 from .expression.base import BaseExpression
 from .expression.call import CallExpression
+from .expression.access import AccessExpression
+from .expression.assignment import AssignmentExpression
--- pjscript/models/base.py
+++ pjscript/models/base.py
@@ -1,6 +1,12 @@
 """PJScript BaseModel"""
 
+# pylint: disable=R0903
+
 
 class BaseModel:
 
     """BaseModel class"""
+
+    def to_dict(self) -> dict:
+
+        """Returns dict representation"""
