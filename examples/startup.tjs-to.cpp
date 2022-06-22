99e6333cf550b2f2f23f5b5197cef3bdb80287df
Stoian Minaiev <stoyan.minaev@gmail.com>
2022-06-22 22:26:55 +0300

    Update CXX example of startup.tjs

--- examples/startup.tjs-to.cpp
+++ examples/startup.tjs-to.cpp
@@ -4,10 +4,12 @@ Value& startup(Environment& _env) {
     _env.set("xyz", StringValue("This gonna be treated as 'var xyz = ...' form"), mutable=true);
     _env.get("xyz");
     _env.set("foo", ObjectValue(), mutable=true);
-    _env.set("foo", ObjectValue(), mutable=true);
-    _env.set("foo", ObjectValue::AsInstance(), mutable=true);
+    _env.set("foo", ObjectValue::asInstance(), mutable=true);
+    _env.set("foo", ObjectValue::asInstance(), mutable=true);
     _env.set("bar", _env.get("foo"), mutable=false);
     _env.get("bar").set("name", StringValue("User"), mutable=true);
     _env.get("bar").get("name");
-    return Console::Log(StringValue("Hello, ", _env.get("bar").get("name"), _env.get("bar").get("name").toString()));
+    // todo: we should also handle arithmetical operations here as well
+    Console::Log(StringValue("Hello, ", _env.get("bar").get("name"), _env.get("bar").get("name").toString()));
+    return NullValue();
 }
