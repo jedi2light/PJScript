bbb503e22bec81dd9bf6729c8f4e7a3a9743e9e5
Stoian Minaiev <stoyan.minaev@gmail.com>
2022-06-26 02:11:44 +0300

    Update reference

--- examples/_reference/program.cpp
+++ examples/_reference/program.cpp
@@ -1,12 +1,9 @@
-// explictily include 'PJScript' complete header.
-#include "../../runtime/cxx/pjscript.hpp"
-
 // each module has its own header: <name>-to.hpp.
 #include "startup.tjs-to.hpp"
 
 int main() {
     Environment* environment = new Environment();
-    // GUIEnvironment gui = new GUIEnvrionment();
+    // GUIEnvironment gui = new GUIEnvironment();
     // environment->merge(gui);
     startup(environment);
     return 0;
