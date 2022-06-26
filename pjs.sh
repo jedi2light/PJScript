5be92e07f4b7dea2621e37084d3b7b88f6bf185a
Stoian Minaiev <stoyan.minaev@gmail.com>
2022-06-26 18:36:59 +0300

    Make it a bit fancier

--- pjs.sh
+++ pjs.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 # Script which is useful for debugging (or for the new-comers). :^)
+set -e  # if something went wrong, do not continue, stop it instead
 
 if [ -z $1 ]; then echo "E: No project path were given"; exit 1; fi
-
 if [ ! -d $1 ]; then echo "E: Can not locate a project"; exit 1; fi
 
-make runtime && ./pjs.py --buildproj $1 && "./$1/$(basename $1).sh"
+echo "[1/3]: [Building PJScript runtime]" && make runtime>/dev/null
+echo "[2/3]: [Building project]"&&./pjs.py --buildproj $1>/dev/null
+echo "[3/3]: [Executing a built project]"&&"./$1/$(basename $1).sh"
