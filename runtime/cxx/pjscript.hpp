ed52531f0ee6d46538effc5698a3b3a16f7a4294
Stoian Minaiev <stoyan.minaev@gmail.com>
2022-06-29 00:19:20 +0300

    A bit of refactoring there :D

--- runtime/cxx/pjscript.hpp
+++ runtime/cxx/pjscript.hpp
@@ -4,9 +4,10 @@
 #include "pjscript/objects/object.hpp"
 #include "pjscript/objects/string.hpp"
 #include "pjscript/objects/boolean.hpp"
+// Environ
+#include "pjscript/objects/environment.hpp"
 // Primitives
 #include "pjscript/primitives/null.hpp"
 #include "pjscript/primitives/string.hpp"
 #include "pjscript/primitives/boolean.hpp"
-// Global objects
-#include "pjscript/objects/environment.hpp"
+#include "pjscript/primitives/undefined.hpp"
