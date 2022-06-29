5263dbbff758e407e775936e135dbbd83be20070
Stoian Minaiev <stoyan.minaev@gmail.com>
2022-06-29 23:36:30 +0300

    Refactor to unhardcode environment to use and be able to capture parent object in functions (but it could be ... useless... do not exactly know...)

--- pjscript/models/expression/base.py
+++ pjscript/models/expression/base.py
@@ -13,49 +13,63 @@ class BaseExpression(BaseModel):
     """Base expression class"""
 
     @staticmethod
-    def _get_gen(identifier: IdentifierLiteral):
+    def _get_gen(
+            identifier: IdentifierLiteral,
+            env: str = '_env'
+    ):
 
-        """Generate _env->get(...)"""
+        """Generate ...env...->get(...)"""
 
-        return f'_env->get({identifier.generate()})'
+        return f'{env}->get({identifier.generate()})'
 
     @staticmethod
-    def _get_member_gen(identifier: IdentifierLiteral):
+    def _get_member_gen(
+            identifier: IdentifierLiteral,
+            env: str = '_env'
+    ):
 
-        """Generate _env->get(...) (for obj. member)"""
+        """Generate ..env..->get(...) for obj. member"""
 
         parts = identifier.token().value().split('.')
-        resulting = f'_env->get((char*)"{parts[0]}")'
+        resulting = f'{ env }->get((char*)"{parts[0]}")'
 
-        for part in parts[1:]:
-            resulting += f'->get((char*)"{part}")'
+        for identifier in parts[1:]:
+            resulting += f'->get((char*)"{identifier}")'
 
         return resulting
 
     @staticmethod
     def _set_gen(
             lhs: IdentifierLiteral,
-            rhs: BaseModel, mutable: bool):
+            rhs: BaseModel, mutable: bool,
+            env: str = '_env'
+    ):
 
-        """Generate _env->set(...)"""
+        """Generate ...env...->set(...)"""
 
-        return f'_env->set({lhs.generate()}, ' \
+        return f'{{auto* _ = {env};' \
+               f'_->set({lhs.generate()}, ' \
                f'{rhs.generate()}, ' \
-               f'{"true" if mutable else "false"})'
+               f'{ "true" if mutable else "false" });}}'
 
     @staticmethod
     def _set_member_gen(
             lhs: IdentifierLiteral,
-            rhs: BaseModel, mutable: bool):
+            rhs: BaseModel,
+            mutable: bool,
+            env: str = '_env'
+    ):
 
-        """Generate _env->set(...) (for obj. member)"""
+        """Generate ...env...->set(...) for obj. member"""
 
         parts = lhs.token().value().split('.')
-        resulting = f'_env->get((char*)"{parts[0]}")'
+        resulting = f'{{auto* _ = {env}->get((char*)"{parts[0]}")'
 
         for part in parts[1:-1]:
             resulting += f'->get((char*)"{part}")'
 
-        return resulting + f'->set((char*)"{parts[-1]}", ' \
+        resulting += ';'  # <---- separate get and set expressions
+
+        return resulting + f'_->set((char*)"{parts[-1]}", ' \
                            f'{rhs.generate()}, ' \
-                           f'{"true" if mutable else "false"})'
+                           f'{"true" if mutable else "false"});}}'
