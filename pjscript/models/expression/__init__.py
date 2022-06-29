0355b9e4bc3b449e3eeab76188e0271b94267b9f
Stoian Minaiev <stoyan.minaev@gmail.com>
2022-06-29 23:35:07 +0300

    A prt of prev commit

--- pjscript/models/expression/__init__.py
+++ pjscript/models/expression/__init__.py
@@ -2,8 +2,6 @@
 
 # Binary expression
 from .binary import BinaryExpression
-# Return expression
-from .return_ import ReturnExpression
 # Program expression
 from .program import ProgramExpression
 # Function expression
