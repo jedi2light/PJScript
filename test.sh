f7460811240751cd00546d43410ab6c3b0bad305
Stoian Minaiev <stoyan.minaev@gmail.com>
2022-07-04 20:03:59 +0300

    Add handy shortcut for comparing output with Node.js

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# This script helps with testing by comparing a PJScript behaviour to the Node.js one
+
+if [ -z $1 ]; then echo "E: No project path were given"; exit 1; fi
+if [ ! -d $1 ]; then echo "E: Can not locate a project"; exit 1; fi
+
+RESULT="${1}/generated/pjs-vs-node.diff"  # set path to a diff file
+
+diff <(./pjs.sh $1 | sed -e '1,3d' -e 's/ $//g') <(node $1/startup.tjs) > "${RESULT}"
+
+if [ "${KOMPARE}" == "1" ]; then kompare ${RESULT}; fi # optionally view with Kompare
